e95d7976216b51ce923ff24e7bb39154ee8185a8
Evan Broscious <broscious@github.com>
2018-07-01 15:44:44 -0400

    jesus, finally got the autopep8 to auto remove lines, probably because it wasn't installed, end me please

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,7 +18,8 @@
   elpy
   flycheck
   magit
-  py-autopep8))
+  py-autopep8
+  ws-butler))
 
 (mapc #'(lambda (package)
     (unless (package-installed-p package)
@@ -41,11 +42,14 @@
   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
   (add-hook 'elpy-mode-hook 'flycheck-mode))
 
+(require 'ws-butler)
+(add-hook 'prog-mode-hook 'ws-butler-mode) ;; unfortunately, this gets broken by the autopep8 hook for some reason but I don't really care enough to figure out how to fix it
+
 ;; enable autopep8 formatting on save
 (require 'py-autopep8)
-(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+(add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
 
-(setq-default show-trailing-whitespace t)
+;; (setq-default show-trailing-whitespace t)
 
 (setq
    backup-by-copying t      ; don't clobber symlinks
