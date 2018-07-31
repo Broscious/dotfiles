a022cce7d04d5cf88c8340406d7a5f28f2781cb7
Evan Broscious <broscious@github.com>
2018-07-30 22:57:59 -0400

    added missing config to set filetypes to be supported by web mode and set support for jsx

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -52,6 +52,21 @@
 
 ;; (setq-default show-trailing-whitespace t)
 
+;; setup webmode for the following file types
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js[x]?\\'" . web-mode))
+
+(setq web-mode-content-types-alist
+  '(("jsx" . "\\.js[x]?\\'")))
+
 (setq
    backup-by-copying t      ; don't clobber symlinks
    backup-directory-alist
